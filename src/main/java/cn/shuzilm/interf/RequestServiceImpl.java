a158a51d6401b7ceeadced1822fd043233e3a519
houkp <houkepei@bonc.com.cn>
2018-07-11 20:33:53 +0800

    添加log日志

--- src/main/java/cn/shuzilm/interf/RequestServiceImpl.java
+++ src/main/java/cn/shuzilm/interf/RequestServiceImpl.java
@@ -9,14 +9,15 @@ import org.slf4j.LoggerFactory;
 public class RequestServiceImpl implements RequestService {
 
     private static final Logger log = LoggerFactory.getLogger(RequestServiceImpl.class);
+
     @Override
     public BidRequestBean parseRequest(String dataStr) {
-        log.debug(" BidRequest参数入参：{}",dataStr);
+        log.debug(" BidRequest参数入参：{}", dataStr);
         if (StringUtils.isNotBlank(dataStr)) {
             JSONObject jsonObject = JSONObject.fromObject(dataStr);
-            log.debug(" jsonObject的结果：{}",jsonObject);
+            log.debug(" jsonObject的结果：{}", jsonObject);
             BidRequestBean bidRequestBean = (BidRequestBean) JSONObject.toBean(jsonObject, BidRequestBean.class);
-            log.debug(" json转化为BidRequestBean的结果：{}",bidRequestBean);
+            log.debug(" json转化为BidRequestBean的结果：{}", bidRequestBean);
             return bidRequestBean;
         }
         return null;
