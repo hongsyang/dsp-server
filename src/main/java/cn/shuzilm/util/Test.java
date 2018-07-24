7ce38732d3735e2a6c10cb69b5b07b487acdcde8
houkp <houkepei@bonc.com.cn>
2018-07-24 12:01:55 +0800

    Phoenix jar包引入

--- src/main/java/cn/shuzilm/util/Test.java
+++ src/main/java/cn/shuzilm/util/Test.java
@@ -4,13 +4,38 @@ import cn.shuzilm.interf.rtb.parser.RequestService;
 import cn.shuzilm.interf.rtb.parser.RequestServiceFactory;
 import org.reflections.Reflections;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicReference;
 
 
 public class Test {
+    private static String driver = "org.apache.phoenix.jdbc.PhoenixDriver";
+    public static void main(String[] args) throws SQLException {
+        try {
 
-    public static void main(String[] args) {
+            Class.forName("org.apache.phoenix.jdbc.PhoenixDriver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        Statement stmt = null;
+        ResultSet rs = null;
+
+        Connection con = DriverManager.getConnection("jdbc:phoenix:hadoop0,hadoop1,hadoop2:2181");
+        stmt = con.createStatement();
+        String sql = "select * from test";
+        rs = stmt.executeQuery(sql);
+        while (rs.next()) {
+            System.out.print("id:"+rs.getString("id"));
+            System.out.println(",name:"+rs.getString("name"));
+        }
+        stmt.close();
+        con.close();
+
 //        DUFlowBean duFlowBean =new DUFlowBean();
 //        duFlowBean.setRequestId("11111111111111111111111");
 //        DUFlowBean duFlowBean1 =new DUFlowBean();
@@ -21,27 +46,27 @@ public class Test {
 //        System.out.println("这是第二个"+duFlowBean1);
 
 
-        Reflections reflections = new Reflections("cn.shuzilm.interf.rtb.parser");
-
-        Set<Class<? extends RequestService>> monitorClasses = reflections.getSubTypesOf(RequestService.class);
+//        Reflections reflections = new Reflections("cn.shuzilm.interf.rtb.parser");
+//
+//        Set<Class<? extends RequestService>> monitorClasses = reflections.getSubTypesOf(RequestService.class);
 //        player.toString().contains("Lingji")
 //        monitorClasses.forEach((player) -> RequestServiceFactory.getRequestService(player.getName()) );
 //        if (player.toString().toLowerCase().contains("lingji"))
-        String data ="11";
-        AtomicReference<String> className1 =null;
-        monitorClasses.forEach(player ->{
-            System.out.println(player.toString().toLowerCase().contains("lingji"));
-            if (player.toString().toLowerCase().contains("lingji")){
-                className1.set(player.getName());
-            }
-        });
-        String className =null;
-        for (Class<? extends RequestService> player : monitorClasses) {
-            System.out.println(player.toString().toLowerCase().contains("lingji"));
-            if (player.toString().toLowerCase().contains("lingji")) {
-                className=player.getName() ;
-            }
-        }
-        String s = RequestServiceFactory.getRequestService(className).parseRequest(className);
+//        String data ="11";
+//        AtomicReference<String> className1 =null;
+//        monitorClasses.forEach(player ->{
+//            System.out.println(player.toString().toLowerCase().contains("lingji"));
+//            if (player.toString().toLowerCase().contains("lingji")){
+//                className1.set(player.getName());
+//            }
+//        });
+//        String className =null;
+//        for (Class<? extends RequestService> player : monitorClasses) {
+//            System.out.println(player.toString().toLowerCase().contains("lingji"));
+//            if (player.toString().toLowerCase().contains("lingji")) {
+//                className=player.getName() ;
+//            }
+//        }
+//        String s = RequestServiceFactory.getRequestService(className).parseRequest(className);
     }
 }
