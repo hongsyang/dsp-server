16035783594e6fe725bf765294839ab640970ec1
houkp <houkepei@bonc.com.cn>
2018-07-27 16:38:35 +0800

    pixel 配置信息

--- src/main/java/cn/shuzilm/bean/control/AdPixelBean.java
+++ src/main/java/cn/shuzilm/bean/control/AdPixelBean.java
@@ -1,13 +1,16 @@
 package cn.shuzilm.bean.control;
 
+import lombok.Data;
+
 /**
  * 从 PIXEL 服务器获得的 win notice 和 点击请求
  *
  * Created by thunders on 2018/7/11.
  */
+@Data
 public class AdPixelBean implements ICommand{
-    private String host;
-    private String adUid;
+    private String host;//根据配置文件获取
+    private String adUid;//广告id
     private String adName;
     /**
      * 实际耗费的金额
@@ -17,44 +20,4 @@ public class AdPixelBean implements ICommand{
      * 实际耗费的量
      */
     private long winNoticeNums;
-
-    public String getHost() {
-        return host;
-    }
-
-    public void setHost(String host) {
-        this.host = host;
-    }
-
-    public String getAdUid() {
-        return adUid;
-    }
-
-    public void setAdUid(String adUid) {
-        this.adUid = adUid;
-    }
-
-    public String getAdName() {
-        return adName;
-    }
-
-    public void setAdName(String adName) {
-        this.adName = adName;
-    }
-
-    public float getMoney() {
-        return money;
-    }
-
-    public void setMoney(float money) {
-        this.money = money;
-    }
-
-    public long getWinNoticeNums() {
-        return winNoticeNums;
-    }
-
-    public void setWinNoticeNums(long winNoticeNums) {
-        this.winNoticeNums = winNoticeNums;
-    }
 }
--- src/main/java/cn/shuzilm/interf/pixcel/parser/AdViewExpParser.java
+++ src/main/java/cn/shuzilm/interf/pixcel/parser/AdViewExpParser.java
@@ -30,18 +30,28 @@ public class AdViewExpParser  implements  ParameterParser{
 
     @Override
     public String parseUrl(String url) {
-        MDC.put("sift","rtb");
         DUFlowBean duFlowBean = new DUFlowBean();
         Map<String, String> urlRequest = UrlParserUtil.urlRequest(url);
-        log.debug("urlRequest:{}",urlRequest);
+        MDC.put("sift","AdViewWurl");
+        log.debug("AdViewExp曝光的wurl值:{}",urlRequest);
+        String requestId = urlRequest.get("req");
         Object element = JedisQueueManager.getElementFromQueue(urlRequest.get("req"));
-        log.debug("element:{}",element);
+        log.debug("AdViewExp曝光的requestid:{},wurl值:{}:[]",requestId,element);
+        MDC.put("sift","pixel");
         AdPixelBean bean = new AdPixelBean();
+        bean.setAdName(urlRequest.get(""));
+        log.debug("发送到pixel服务器的AdPixelBean：{}",bean);
         PixelFlowControl.getInstance().sendStatus(bean);
-        duFlowBean.setInfoId( urlRequest.get("houkp")+ UUID.randomUUID());
+        duFlowBean.setInfoId( urlRequest.get("req")+ UUID.randomUUID());
+        MDC.put("sift","AdViewExp");
+        log.debug("\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}", duFlowBean.getInfoId(),
+                duFlowBean.getDid(), duFlowBean.getDeviceId(),
+                duFlowBean.getAdUid(),duFlowBean.getAdvertiserUid(),
+                duFlowBean.getAdvertiserUid(), duFlowBean.getAgencyUid(),
+                duFlowBean.getCreativeUid(), duFlowBean.getProvince(),
+                duFlowBean.getCity(),duFlowBean.getRequestId());
         String duFlowBeanJson = JSON.toJSONString(duFlowBean);
         log.debug("duFlowBeanJson:{}",duFlowBeanJson);
-        MDC.remove("sift");
         return duFlowBeanJson;
     }
 }
