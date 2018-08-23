e44ff577e84eb8b14724a07f458ed739e7fdc56f
duanyj <duanyj@shuzilm.cn>
2018-08-23 16:35:36 +0800

    pixel获取最终消耗金额

--- src/main/java/cn/shuzilm/backend/pixel/PixelFlowControl.java
+++ src/main/java/cn/shuzilm/backend/pixel/PixelFlowControl.java
@@ -1,15 +1,17 @@
 package cn.shuzilm.backend.pixel;
 
+import java.util.ArrayList;
+
 import cn.shuzilm.backend.master.MsgControlCenter;
+import cn.shuzilm.bean.control.AdBean;
 import cn.shuzilm.bean.control.AdPixelBean;
+import cn.shuzilm.common.Constants;
 
 /**
  * Created by thunders on 2018/7/17.
  */
 public class PixelFlowControl {
     private static PixelFlowControl pixcel = null;
-    private PixelFlowControl(){
-    }
 
     public static PixelFlowControl getInstance(){
         if(pixcel == null)
@@ -18,11 +20,11 @@ public class PixelFlowControl {
     }
     
     private String nodeName;
-    public PixelFlowControl(String nodeName){
-        this.nodeName = nodeName;
+    public PixelFlowControl(){
+        this.nodeName = Constants.getInstance().getConf("HOST");;
     }
 
-    public void sendStatus(AdPixelBean pixel){
+    public void sendStatus(AdPixelBean pixel){   	
         MsgControlCenter.sendPixelStatus(this.nodeName,pixel);
     }
 
@@ -31,5 +33,13 @@ public class PixelFlowControl {
         PixelFlowControl.getInstance().sendStatus(bean);
     }
 
+    /**
+     * 每隔 10 分钟更新一次广告素材或者人群包
+     */
+    public void pullTenMinutes() {
+    	// 从 10 分钟的队列中获得广告素材和人群包
+    	ArrayList<AdBean> adBeanList = MsgControlCenter.recvAdBean(nodeName);
+    	
+    }
 
 }
