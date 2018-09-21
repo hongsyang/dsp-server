9b38e94ec073e48d979eb660e7caee176a2ba738
houkp <houkp@shuzilm.cn>
2018-09-21 11:53:27 +0800

    Merge branch 'duanyj' of http://git.du.com/du-da/dsp-server into Rtb、Pixe
    # Conflicts:
    #	src/main/resources/configs_redis.properties
    #	src/main/resources/rtb.properties

--- src/main/java/cn/shuzilm/util/MD5Util.java
+++ src/main/java/cn/shuzilm/util/MD5Util.java
@@ -27,16 +27,17 @@ public class MD5Util {
     }
 
     public static void main(String[] args) {
-        System.out.println("品牌：oppo r7007;"+"操作系统：Android;IMEI:"+MD5("866089025831526"));
-        System.out.println("品牌：山寨苹果;"+"操作系统：Android;IMEI"+MD5("350998832332580"));
-        System.out.println("品牌：Lenovo K30;"+"操作系统：Android;IMEI"+MD5("867792029921198"));
-        System.out.println("品牌：小米NOTE4;"+"操作系统：Android;IMEI"+MD5("861206037426724"));
-        System.out.println("品牌：红米2A;"+"操作系统：Android;IMEI"+MD5("868942026149793"));
-        System.out.println("品牌：中兴远航3;"+"操作系统：Android;IMEI"+MD5("868153022515488"));
-        System.out.println("品牌：小辣椒;"+"操作系统：Android;IMEI"+MD5("867338031004605"));
-        System.out.println("品牌：魅族;"+"操作系统：Android;IMEI"+MD5("867900916074530"));
+//        System.out.println("品牌：oppo r7007;"+"操作系统：Android;IMEI:"+MD5("866089025831526"));
+//        System.out.println("品牌：山寨苹果;"+"操作系统：Android;IMEI"+MD5("350998832332580"));
+//        System.out.println("品牌：Lenovo K30;"+"操作系统：Android;IMEI"+MD5("867792029921198"));
+//        System.out.println("品牌：小米NOTE4;"+"操作系统：Android;IMEI"+MD5("861206037426724"));
+//        System.out.println("品牌：红米2A;"+"操作系统：Android;IMEI"+MD5("868942026149793"));
+//        System.out.println("品牌：中兴远航3;"+"操作系统：Android;IMEI"+MD5("868153022515488"));
+//        System.out.println("品牌：小辣椒;"+"操作系统：Android;IMEI"+MD5("867338031004605"));
+//        System.out.println("品牌：魅族;"+"操作系统：Android;IMEI"+MD5("867900916074530"));
 //        System.out.println("品牌：oppo r7007;"+"操作系统：Android;"+MD5("866089025831526"));
 //        System.out.println("品牌：oppo r7007;"+"操作系统：Android;"+MD5("866089025831526"));
+        System.out.println("token;"+MD5("houkp@shuzilm.cncwxk9c2220wkpjvvughbrypay9k1x50o"));
     }
 }
 
