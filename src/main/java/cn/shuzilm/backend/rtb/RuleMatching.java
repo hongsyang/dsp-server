8c95e6c29d7bbe068082fbe920b4fb6b03b7440e
duanyj <duanyj@shuzilm.cn>
2018-10-17 11:24:28 +0800

    异常抛出

--- src/main/java/cn/shuzilm/backend/rtb/RuleMatching.java
+++ src/main/java/cn/shuzilm/backend/rtb/RuleMatching.java
@@ -120,7 +120,7 @@ public class RuleMatching {
 	 */
 	public boolean filter(int width, int height, int adWidth, int adHeight, boolean isResolutionRatio,
 			int widthDeviation, int heightDeviation, String adxName, Material material, String extStr,
-			Set<String> materialSet) {
+			Set<String> materialSet) throws Exception{
 		// 筛选审核通过的物料
 		if (material.getApproved_adx() != null && !material.getApprovedAdxSet().contains(adxName)) {
 			return false;
@@ -161,7 +161,7 @@ public class RuleMatching {
 	 *            高度误差
 	 */
 	public DUFlowBean match(String deviceId, String adType, int width, int height, boolean isResolutionRatio,
-			int widthDeviation, int heightDeviation, String adxName, String extStr) {
+			int widthDeviation, int heightDeviation, String adxName, String extStr) throws Exception{
 		MDC.put("sift", "rtb");
 		DUFlowBean targetDuFlowBean = null;
 		if (deviceId == null || deviceId.trim().equals("")) {
@@ -412,7 +412,7 @@ public class RuleMatching {
 	 * 对匹配的广告按照规则进行排序
 	 */
 	public DUFlowBean order(Map<String, Material> metrialMap, String deviceId, List<AdBean> machedAdList,
-			TagBean tagBean, String widthHeightRatio, List<String> tagIdList, Map<String, AudienceBean> audienceMap,String adxName) {
+			TagBean tagBean, String widthHeightRatio, List<String> tagIdList, Map<String, AudienceBean> audienceMap,String adxName) throws Exception{
 		MDC.put("sift", "rtb");
 		DUFlowBean targetDuFlowBean = null;
 		List<AdBean> gradeList = new ArrayList<AdBean>();
@@ -751,8 +751,12 @@ public class RuleMatching {
 	}
 
 	public static void main(String[] args) {
+		try{
 		RuleMatching rule = RuleMatching.getInstance();
 		rule.match("72229B9518E18744620932CB50FC43DC", "feed", 720, 240, true, 5, 5, "2", "jpg,gif");
+		}catch(Exception e){
+			e.getMessage();
+		}
 	}
 
 }
