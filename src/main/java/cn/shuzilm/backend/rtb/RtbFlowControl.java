21c7ef61f148db40f5c19916b3d318004bc0fb8d
duanyj <duanyj@shuzilm.cn>
2018-10-19 12:23:14 +0800

    广告增加开始和结束的时间的匹配

--- src/main/java/cn/shuzilm/backend/rtb/RtbFlowControl.java
+++ src/main/java/cn/shuzilm/backend/rtb/RtbFlowControl.java
@@ -467,6 +467,12 @@ public class RtbFlowControl {
         // 匹配广告投放时间窗
         AdBean adBean = mapAd.get(auid);
         if (adBean != null) {
+        	long startTime = adBean.getStartTime().getTime();
+        	long endTime = adBean.getEndTime().getTime();
+        	long now = System.currentTimeMillis()/1000;
+        	if(startTime > now || endTime < now){
+        		return false;
+        	}
             int[][] timeSchedulingArr = adBean.getTimeSchedulingArr();
             if(timeSchedulingArr != null){
             for (int i = 0; i < timeSchedulingArr.length; i++) {
