7ad5e345ca2a3c47957a63e7fa32dfef4ed005a0
houkp <houkp@shuzilm.cn>
2018-11-23 14:19:33 +0800

    广告匹配规则添加

--- src/main/java/cn/shuzilm/interf/rtb/parser/YouYiRequestServiceImpl.java
+++ src/main/java/cn/shuzilm/interf/rtb/parser/YouYiRequestServiceImpl.java
@@ -1,5 +1,7 @@
 package cn.shuzilm.interf.rtb.parser;
 
+import com.google.common.collect.Lists;
+
 import bidserver.BidserverSsp;
 import cn.shuzilm.backend.rtb.RuleMatching;
 import cn.shuzilm.bean.internalflow.DUFlowBean;
@@ -58,204 +60,92 @@ public class YouYiRequestServiceImpl implements RequestService {
             YouYiBidRequest bidRequestBean = JSON.parseObject(dataStr, YouYiBidRequest.class);
             //创建返回结果  bidRequest请求参数保持不变
             YouYiMobile userDevice = bidRequestBean.getMobile();//设备APP信息
-            List<YouYiAdzone> adzone = bidRequestBean.getAdzone();//曝光信息
+            YouYiAdzone adzone = bidRequestBean.getAdzone().get(0);//曝光信息
             YouYiUser user = bidRequestBean.getUser();//用户信息
 
-//            App app = bidRequestBean.getApp();//应用信息
-//            Integer width = null;//广告位的宽
-//            Integer height = null;//广告位的高
+            Integer width = null;//广告位的宽
+            Integer height = null;//广告位的高
 //            Integer showtype = userImpression.getExt().getShowtype();//广告类型
-//            String adType = convertAdType(showtype); //对应内部 广告类型
-//            String stringSet = null;//文件类型列表
-//            String deviceId = null;//设备号
-//
-//
+            String adType = null; //对应内部 广告类型
+            String stringSet = null;//文件类型列表
+            String deviceId = null;//设备号
 //            if (StringUtils.isBlank(adType)) {
 //                response = "没有对应的广告类型";
 //                return response;
 //
 //            }
+
+
 //            //设备的设备号：用于匹配数盟库中的数据
-//            if (userDevice != null) {
-//                if ("ios".equals(userDevice.getOs().toLowerCase())) {
-//                    deviceId = userDevice.getExt().getIdfa();
-//                } else if ("android".equalsIgnoreCase(userDevice.getOs().toLowerCase())) {
-////                    deviceId = userDevice.getExt().getMac();
-//                    deviceId = userDevice.getDidmd5();
-//                } else if ("wp".equals(userDevice.getOs().toLowerCase())) {
-////                    deviceId = userDevice.getExt().getMac();
-//                    deviceId = userDevice.getDidmd5();
-//                }
-//            }
+            if (userDevice != null) {
+                if ("ios".equals(userDevice.getDevice_os().toLowerCase())) {
+                    deviceId = userDevice.getIdfa();
+                } else if ("android".equalsIgnoreCase(userDevice.getDevice_os().toLowerCase())) {
+//                    deviceId = userDevice.getExt().getMac();
+                    deviceId = userDevice.getMd5_imei();
+                }
+            }
 //            //支持的文件类型
-//            List<LJAssets> assets = new ArrayList<>();
-//            if ("banner".equals(adType)) {// banner 类型
-//                width = userImpression.getBanner().getW();
-//                height = userImpression.getBanner().getH();
-//                String[] mimes = userImpression.getBanner().getMimes();//文件扩展名列表
-//                stringSet = Arrays.toString(mimes);
-//
-//            } else if ("fullscreen".equals(adType)) { //开屏
-//                if (userImpression.getVideo() != null) {
-//                    width = userImpression.getVideo().getW();
-//                    height = userImpression.getVideo().getH();
-//                    String[] mimes = userImpression.getVideo().getMimes();//文件扩展名列表
-//                    stringSet = Arrays.toString(mimes);
-//                } else if (userImpression.getBanner() != null) {
-//                    width = userImpression.getBanner().getW();
-//                    height = userImpression.getBanner().getH();
-//                    String[] mimes = userImpression.getBanner().getMimes();//文件扩展名列表
-//                    stringSet = Arrays.toString(mimes);
-//                }
-//            } else if ("interstitial".equals(adType)) {//插屏
-//                if (userImpression.getVideo() != null) {
-//                    width = userImpression.getVideo().getW();
-//                    height = userImpression.getVideo().getH();
-//                    String[] mimes = userImpression.getVideo().getMimes();//文件扩展名列表
-//                    stringSet = Arrays.toString(mimes);
-//                } else if (userImpression.getBanner() != null) {
-//                    width = userImpression.getBanner().getW();
-//                    height = userImpression.getBanner().getH();
-//                    String[] mimes = userImpression.getBanner().getMimes();//文件扩展名列表
-//                    stringSet = Arrays.toString(mimes);
-//                }
-//
-//            } else if ("feed".equals(adType)) { //信息流
-//                assets = userImpression.getNativead().getAssets();
-//                for (LJAssets asset : assets) {
-//                    if (asset.getImg() != null && asset.getRequired().equals(true)) {
-//                        width = asset.getImg().getW();
-//                        height = asset.getImg().getH();
-//                        stringSet = Arrays.toString(asset.getImg().getMimes());
-//                    } else if (asset.getVideo() != null && asset.getRequired().equals(true)) {
-//                        width = asset.getVideo().getW();
-//                        height = asset.getVideo().getH();
-//                        stringSet = Arrays.toString(asset.getVideo().getMimes());
-//                    }
-//
-//                }
-//            }
-//
-//
-//            //初步过滤规则开关
-//            if (Boolean.valueOf(configs.getString("FILTER_SWITCH"))) {
-//                if (FilterRule.filterRuleBidRequest(bidRequestBean, true, msg, ADX_NAME)) {
-//                    DUFlowBean targetDuFlowBean = ruleMatching.match(
-//                            deviceId,//设备mac的MD5
-//                            adType,//广告类型
-//                            width,//广告位的宽
-//                            height,//广告位的高
-//                            true,// 是否要求分辨率
-//                            5,//宽误差值
-//                            5,// 高误差值;
-//                            ADX_ID,//ADX 服务商ID
-//                            stringSet,//文件扩展名
-//                            userDevice.getIp()//用户ip
-//                    );
-//                    if (targetDuFlowBean == null) {
-//                        response = "";
-//                        return response;
-//                    }
-//                    MDC.put("sift", configs.getString("ADX_REQUEST"));
-//                    //需要添加到Phoenix中的数据
-//                    targetDuFlowBean.setRequestId(bidRequestBean.getId());//bidRequest id
-//                    targetDuFlowBean.setImpression(bidRequestBean.getImp());//曝光id
-//                    targetDuFlowBean.setAdxSource(ADX_NAME);//ADX服务商渠道
-//                    targetDuFlowBean.setAdTypeId(adType);//广告大类型ID
-//                    targetDuFlowBean.setAdxAdTypeId(showtype);//广告小类对应ADX服务商的ID
-//                    targetDuFlowBean.setAdxId(ADX_ID);//ADX广告商id
-//                    targetDuFlowBean.setBidid(MD5Util.MD5(MD5Util.MD5(bidRequestBean.getId())));//bid id
-//                    targetDuFlowBean.setDspid(LocalDateTime.now().toString() + UUID.randomUUID());//dsp id
-//                    targetDuFlowBean.setAppName(app.getName());//APP名称
-//                    targetDuFlowBean.setAppPackageName(app.getBundle());//APP包名
-//                    if (app.getExt() != null) {
-//                        targetDuFlowBean.setAppVersion(app.getExt().getSdk() == null ? "" : app.getExt().getSdk());//APP版本
-//                    }
-//
-//                    log.debug("过滤通过的targetDuFlowBean:{}", targetDuFlowBean);
-//                    BidResponseBean bidResponseBean = convertBidResponse(targetDuFlowBean, adType, assets);
-//                    MDC.remove("sift");
-////                    pushRedis(targetDuFlowBean);//上传到redis服务器
-//                    response = JSON.toJSONString(bidResponseBean);
-//                    log.debug("过滤通过的bidResponseBean:{}", response);
-//                } else {
-//                    response = JSON.toJSONString(msg);//过滤规则结果输出
-//                }
-//
-//            } else {
-//                DUFlowBean targetDuFlowBean = ruleMatching.match(
-//                        deviceId,//设备mac的MD5
-//                        adType,//广告类型
-//                        width,//广告位的宽
-//                        height,//广告位的高
-//                        true,// 是否要求分辨率
-//                        5,//宽误差值
-//                        5,// 高误差值;
-//                        ADX_ID,//ADX 服务商ID
-//                        stringSet,//文件扩展名
-//                        userDevice.getIp()//用户ip
-//                );
-//                if (targetDuFlowBean == null) {
-//                    response = "";
-//                    return response;
-//                }
-//                MDC.put("sift", "dsp-server");
-//                log.debug("bidRequestBean.id:{}", bidRequestBean.getId());
-//                //需要添加到Phoenix中的数据
-//                targetDuFlowBean.setRequestId(bidRequestBean.getId());//bidRequest id
-//                targetDuFlowBean.setImpression(bidRequestBean.getImp());//曝光id
-//                targetDuFlowBean.setAdxSource(ADX_NAME);//ADX服务商渠道
-//                targetDuFlowBean.setAdTypeId(adType);//广告大类型ID
-//                targetDuFlowBean.setAdxAdTypeId(showtype);//广告小类对应ADX服务商的ID
-//                targetDuFlowBean.setAdxId(ADX_ID);//ADX广告商id
-//                targetDuFlowBean.setBidid(MD5Util.MD5(MD5Util.MD5(bidRequestBean.getId())));//bid id
-//                targetDuFlowBean.setDspid(LocalDateTime.now().toString() + UUID.randomUUID());//dsp id
-//                targetDuFlowBean.setAppName(app.getName());//APP名称
-//                targetDuFlowBean.setAppPackageName(app.getBundle());//APP包名
-//                targetDuFlowBean.setAppId(app.getId());//APP包名
-//                if (app.getExt() != null) {
-//                    targetDuFlowBean.setAppVersion(app.getExt().getSdk() == null ? "" : app.getExt().getSdk());//APP版本
-//                }
-//
-//
-//                log.debug("没有过滤的targetDuFlowBean:{}", targetDuFlowBean);
-//                BidResponseBean bidResponseBean = convertBidResponse(targetDuFlowBean, adType, assets);
-//                MDC.remove("sift");
-////                pushRedis(targetDuFlowBean);//上传到redis服务器
-//                response = JSON.toJSONString(bidResponseBean);
-//                MDC.put("sift", "dsp-server");
-//                log.debug("没有过滤的bidResponseBean:{}", response);
-//            }
+            String adz_type = adzone.getAdz_type();
+            if (adz_type.equals("ADZONE_TYPE_INAPP_BANNER") | adz_type.equals("ADZONE_TYPE_WAP_BANNER")) {
+                stringSet = "[image/jpeg, image/png]";
+                //广告位的宽和高
+                width = adzone.getAdz_width();
+                height = adzone.getAdz_height();
+            } else if (adz_type.equals("ADZONE_TYPE_INAPP_VIDEO") | adz_type.equals("ADZONE_TYPE_WAP_VIDEO")) {
+                stringSet = "[ application/x-shockwave-flash，video/x-flv]";
+                //广告位的宽和高
+                width = adzone.getAdz_width();
+                height = adzone.getAdz_height();
+            } else if (adz_type.equals("ADZONE_TYPE_INAPP_NATIVE")) {
+                stringSet = "[image/jpeg, image/png]";
+            }
+
+            //广告匹配规则
+            DUFlowBean targetDuFlowBean = ruleMatching.match(
+                    deviceId,//设备mac的MD5
+                    adType,//广告类型
+                    width,//广告位的宽
+                    height,//广告位的高
+                    true,// 是否要求分辨率
+                    5,//宽误差值
+                    5,// 高误差值;
+                    ADX_ID,//ADX 服务商ID
+                    stringSet,//文件扩展名
+                    user.getUser_ip(),//用户ip
+                    userDevice.getApp_bundle()//APP包名
+            );
+            if (targetDuFlowBean == null) {
+                response = "";
+                return response;
+            }
+            YouYiBidResponse bidResponseBean = convertBidResponse(targetDuFlowBean, bidRequestBean);
+            response = JSON.toJSONString(bidResponseBean);
+            MDC.put("sift", "dsp-server");
+            log.debug("bidResponseBean:{}", response);
             return response;
         } else {
             return response;
         }
-
-
     }
-
 
     /**
      * 内部流转DUFlowBean  转换为  BidResponseBean 输出给 ADX服务器
      *
-     * @param duFlowBean
-     * @return
+     * @param targetDuFlowBean
+     * @param bidRequestBean
+     * @return YouYiBidResponse
      */
-    private YouYiBidResponse convertBidResponse(DUFlowBean duFlowBean, YouYiBidRequest bidRequestBean) {
-        YouYiBidResponse bidResponseBean = new YouYiBidResponse();
-        bidResponseBean.setSession_id(bidRequestBean.getSession_id());
-        List<YouYiAd> youYiAds=new ArrayList<>();
-        YouYiAd youYiAd =new YouYiAd();
-        youYiAd.setAdz_id("");
-        youYiAd.setAdz_array_id(0);
-//        youYiAd.setBid_price();
-//        youYiAd.setAdz_id();
-//        youYiAd.setAdz_id();
-//        youYiAds.add();
-        bidResponseBean.setAds(youYiAds);
-        return bidResponseBean;
+    private YouYiBidResponse convertBidResponse(DUFlowBean targetDuFlowBean, YouYiBidRequest bidRequestBean) {
+        YouYiBidResponse youYiBidResponse = new YouYiBidResponse();
+        youYiBidResponse.setSession_id("");
+        youYiBidResponse.setAds(Lists.newArrayList());
+        MDC.put("sift", "bidResponseBean");
+        log.debug("bidResponseBean:{}", JSON.toJSONString(youYiBidResponse));
+        return youYiBidResponse;
 
     }
+
 
     /**
      * 把生成的内部流转DUFlowBean上传到redis服务器 设置5分钟失效
@@ -306,4 +196,6 @@ public class YouYiRequestServiceImpl implements RequestService {
         }
         return adType;
     }
+
+
 }
