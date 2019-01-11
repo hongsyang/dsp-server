8359b508d17142ec586584ef9f2bd8bf38fcd7d4
houkp <houkp@shuzilm.cn>
2019-01-11 11:07:28 +0800

    ExpLogUtil 添加

--- src/main/java/cn/shuzilm/util/ExpLogUtil.java
+++ src/main/java/cn/shuzilm/util/ExpLogUtil.java
@@ -1,15 +1,33 @@
 package cn.shuzilm.util;
 
-import java.io.File;
+import cn.shuzilm.util.aes.AES;
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+
+import java.io.*;
+import java.util.Map;
 
 public class ExpLogUtil {
     public static void main(String[] args) {
         String fileStr = "C:\\Users\\houkp\\Desktop\\exception";
         File file = new File(fileStr);
         File[] files = file.listFiles();
+        int i=0;
         for (File expFile : files) {
             System.out.println(expFile);
-            
+            try {
+                BufferedReader bufferedRead = new BufferedReader(new FileReader(expFile));
+                while (bufferedRead.read() != -1) {
+                    String readLine = bufferedRead.readLine();
+                    if (readLine.contains("elementDUFlowBean")) {
+                        System.out.println(readLine);
+                    }
+                }
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
