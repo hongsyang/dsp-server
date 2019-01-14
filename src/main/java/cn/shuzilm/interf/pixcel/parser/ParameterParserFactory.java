a5e3fb9d6e510af1455b8af24cc431fe3dbcbdc4
houkp <houkp@shuzilm.cn>
2019-01-14 14:29:47 +0800

    反射添加到map中

--- src/main/java/cn/shuzilm/interf/pixcel/parser/ParameterParserFactory.java
+++ src/main/java/cn/shuzilm/interf/pixcel/parser/ParameterParserFactory.java
@@ -9,7 +9,6 @@ package cn.shuzilm.interf.pixcel.parser;
 * @Version:        1.0
 */
 public class ParameterParserFactory {
-   static ParameterParser parameterParser = null;
 
     public static ParameterParser parameterParser = null;
 
