07fbf9d09773776ba1c9358b4702cf4689433925
houkp <houkp@shuzilm.cn>
2019-01-21 11:12:02 +0800

    广点通代码

--- src/main/java/cn/shuzilm/bean/tencent/request/TencentApp.java
+++ src/main/java/cn/shuzilm/bean/tencent/request/TencentApp.java
@@ -1,6 +1,12 @@
 package cn.shuzilm.bean.tencent.request;
 
-import java.io.Serializable;
+import lombok.Data;
 
+import java.io.Serializable;
+@Data
 public class TencentApp  implements Serializable {
+
+    private Integer industry_id;//否 App 所属行业 id
+    private String app_bundle_id;//否 App 的唯一标识。
+
 }
--- src/main/java/cn/shuzilm/bean/tencent/request/TencentUser.java
+++ src/main/java/cn/shuzilm/bean/tencent/request/TencentUser.java
@@ -1,6 +1,13 @@
 package cn.shuzilm.bean.tencent.request;
 
-import java.io.Serializable;
+import lombok.Data;
 
+import java.io.Serializable;
+import java.util.List;
+
+@Data
 public class TencentUser  implements Serializable {
+
+    private  String  id ;//否 Cookie Mapping ID（具体参见文档《腾讯广点通 CookieMapping 服务对接说明文档》）
+    private List<Integer> audience_ids ;//否 用户 ID 命中的 DMP 人群包ID
 }
