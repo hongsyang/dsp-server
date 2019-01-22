a5f24c1cae40f4ceb4bddc80343090e7f6c0929c
houkp <houkp@shuzilm.cn>
2019-01-22 14:21:30 +0800

    imei 和mac的规则过滤

---
+++ src/main/java/cn/shuzilm/interf/pixcel/TencentRedisTask.java
@@ -0,0 +1,48 @@
+package cn.shuzilm.interf.pixcel;
+
+import cn.shuzilm.common.jedis.JedisQueueManager;
+import cn.shuzilm.interf.pixcel.parser.TencentExpParameterParserImpl;
+import cn.shuzilm.interf.pixcel.parser.YouYiExpParameterParserImpl;
+import cn.shuzilm.interf.pixcel.parser.YouYiImpParameterParserImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+* @Description:    悠易解析器
+* @Author:         houkp
+* @CreateDate:     2019/1/15 19:09
+* @UpdateUser:     houkp
+* @UpdateDate:     2019/1/15 19:09
+* @UpdateRemark:   修改内容
+* @Version:        1.0
+*/
+public class TencentRedisTask implements Runnable {
+
+
+    private static final Logger log = LoggerFactory.getLogger(TencentRedisTask.class);
+
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                Object tencentexp = JedisQueueManager.getElementFromQueue("tencentexp");
+                Object tencentimp = JedisQueueManager.getElementFromQueue("tencentimp");
+                if (tencentimp != null) {
+                    log.debug("线程号" + Thread.currentThread().getName());
+                    YouYiImpParameterParserImpl.parseUrlStr(tencentimp.toString());
+                } else if (tencentexp != null) {
+                    log.debug("线程号" + Thread.currentThread().getName());
+                    TencentExpParameterParserImpl.parseUrlStr(tencentexp.toString());
+                }  else {
+                    log.debug("等待中" + Thread.currentThread().getName());
+                    Thread.currentThread().join(1000);
+                }
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+}
+
