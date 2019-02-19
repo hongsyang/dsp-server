a3c3d946c84a010ed22887fa05f3358727a2596c
houkp <houkp@shuzilm.cn>
2019-02-19 16:54:28 +0800

    腾讯价格解析

--- src/main/java/cn/shuzilm/util/ExpLogUtil.java
+++ src/main/java/cn/shuzilm/util/ExpLogUtil.java
@@ -17,26 +17,11 @@ public class ExpLogUtil {
                 BufferedReader bufferedRead = new BufferedReader(new FileReader(expFile));
                 while (bufferedRead.read() != -1) {
                     String readLine = bufferedRead.readLine();
-                    if (readLine.contains("elementDUFlowBean")) {
-                        String exp_error = readLine.substring(readLine.indexOf("DUFlowBean"));
+//                    System.out.println(readLine);
+                    if (readLine.contains("TencentExp曝光的url值:/")) {
+                        String exp_error = readLine.substring(readLine.indexOf("TencentExp曝光的url值:/"));
                         System.out.println(exp_error);
-                        byte[] bytes = null;
-                        ByteArrayOutputStream byteArrayOutputStream = null;
-                        ObjectOutputStream objectOutputStream = null;
-                        byteArrayOutputStream = new ByteArrayOutputStream();
-                        objectOutputStream = new ObjectOutputStream(byteArrayOutputStream);
-                        objectOutputStream.writeObject(exp_error);
-                        objectOutputStream.flush();
-                        bytes = byteArrayOutputStream.toByteArray();
-                        Object obj = null;
-                        ByteArrayInputStream bi = new ByteArrayInputStream(bytes);
-                        ObjectInputStream oi = new ObjectInputStream(bi);
-                        obj = oi.readObject();
-                        bi.close();
-                        oi.close();
-                        System.out.println(obj);
-                        DUFlowBean duFlowBean=new DUFlowBean();
-                        duFlowBean.setRequestId("1");
+
 //                        System.out.println(duFlowBean.toString().getBytes());
 //                        JedisQueueManager.putElementToQueue("EXP", exp_error, Priority.MAX_PRIORITY);
 //                        JSON.parse(duFlowBean);
@@ -45,8 +30,6 @@ public class ExpLogUtil {
             } catch (FileNotFoundException e) {
                 e.printStackTrace();
             } catch (IOException e) {
-                e.printStackTrace();
-            } catch (ClassNotFoundException e) {
                 e.printStackTrace();
             }
         }
