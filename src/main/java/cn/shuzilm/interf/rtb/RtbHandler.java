d5aebef288050ad7df48bfcc82f27303e8eb5dc9
houkp <houkp@shuzilm.cn>
2019-03-04 17:11:27 +0800

    修改灵集204bug

--- src/main/java/cn/shuzilm/interf/rtb/RtbHandler.java
+++ src/main/java/cn/shuzilm/interf/rtb/RtbHandler.java
@@ -169,7 +169,7 @@ public class RtbHandler extends SimpleChannelUpstreamHandler {
 //                response.setStatus(HttpResponseStatus.NO_CONTENT);
                 String substring = resultData.substring(resultData.indexOf("204session_id") + 14);
                 builder.setSessionId(substring);
-                builder.setAds(0, builder.getAds(0));
+                builder.setAds(0, BidserverSsp.BidResponse.Ad.newBuilder().build() );
                 content = builder.build().toByteArray();
             } else if ("".equals(resultData) || "ipBlackList".equals(resultData) || "bundleBlackList".equals(resultData) || "deviceIdBlackList".equals(resultData)) {
                 response.setStatus(HttpResponseStatus.NO_CONTENT);
