f669e66706609713b786a1f1c30d88ae36d7c885
houkp <17611169761@163.com>
2019-03-13 11:59:25 +0800

    添加广告主出价

--- src/main/java/cn/shuzilm/interf/rtb/parser/AdViewRequestServiceImpl.java
+++ src/main/java/cn/shuzilm/interf/rtb/parser/AdViewRequestServiceImpl.java
@@ -442,12 +442,12 @@ public class AdViewRequestServiceImpl implements RequestService {
                 "&adx=" + duFlowBean.getAdxId() +
                 "&did=" + duFlowBean.getDid() +
                 "&device=" + duFlowBean.getDeviceId() +
-                "&app=" + URLEncoder.encode(duFlowBean.getAppName()) +
                 "&appn=" + duFlowBean.getAppPackageName() +
                 "&appv=" + duFlowBean.getAppVersion() +
                 "&pf=" + duFlowBean.getPremiumFactor() +//溢价系数
                 "&ddem=" + duFlowBean.getAudienceuid() + //人群id
                 "&dcuid=" + duFlowBean.getCreativeUid() + // 创意id
+                "&dbidp=" + duFlowBean.getBiddingPrice() +// 广告主出价
                 "&dpro=" + duFlowBean.getProvince() +// 省
                 "&dcit=" + duFlowBean.getCity() +// 市
                 "&dcou=" + duFlowBean.getCountry() +// 县
@@ -456,7 +456,9 @@ public class AdViewRequestServiceImpl implements RequestService {
                 "&daduid=" + duFlowBean.getAdUid() + // 广告id，
                 "&pmp=" + duFlowBean.getDealid() + //私有交易
                 "&dmat=" + duFlowBean.getMaterialId() + //素材id
-                "&userip=" + duFlowBean.getIpAddr();//用户ip
+                "&userip=" + duFlowBean.getIpAddr()+//用户ip
+                "&app=" + URLEncoder.encode(duFlowBean.getAppName()) ;
+
 
         bid.setWurl(wurl);//赢价通知，由 AdView 服务器 发出  编码格式的 CPM 价格*10000，如价格为 CPM 价格 0.6 元，则取值0.6*10000=6000。
 
@@ -471,12 +473,12 @@ public class AdViewRequestServiceImpl implements RequestService {
                 "&adx=" + duFlowBean.getAdxId() +
                 "&did=" + duFlowBean.getDid() +
                 "&device=" + duFlowBean.getDeviceId() +
-                "&app=" + URLEncoder.encode(duFlowBean.getAppName()) +
                 "&appn=" + duFlowBean.getAppPackageName() +
                 "&appv=" + duFlowBean.getAppVersion() +
                 "&pf=" + duFlowBean.getPremiumFactor() +//溢价系数
                 "&ddem=" + duFlowBean.getAudienceuid() + //人群id
                 "&dcuid=" + duFlowBean.getCreativeUid() + // 创意id
+                "&dbidp=" + duFlowBean.getBiddingPrice() +// 广告主出价
                 "&dpro=" + duFlowBean.getProvince() +// 省
                 "&dcit=" + duFlowBean.getCity() +// 市
                 "&dcou=" + duFlowBean.getCountry() +// 县
@@ -485,7 +487,8 @@ public class AdViewRequestServiceImpl implements RequestService {
                 "&daduid=" + duFlowBean.getAdUid() + // 广告id，
                 "&pmp=" + duFlowBean.getDealid() + //私有交易
                 "&dmat=" + duFlowBean.getMaterialId() + //素材id
-                "&userip=" + duFlowBean.getIpAddr();//用户ip
+                "&userip=" + duFlowBean.getIpAddr()+   //用户ip
+                "&app=" + URLEncoder.encode(duFlowBean.getAppName()) ;
 
         Map nurlMap = new HashMap();
         List<String> trackingurls = new ArrayList<>();
