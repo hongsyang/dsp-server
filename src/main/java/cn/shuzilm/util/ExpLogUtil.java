5755ead08e657eff9b2edac9414b5bf27e0a6a71
houkp <17611169761@163.com>
2019-03-25 15:59:02 +0800

    根据行数切分日志

--- src/main/java/cn/shuzilm/util/ExpLogUtil.java
+++ src/main/java/cn/shuzilm/util/ExpLogUtil.java
@@ -25,39 +25,42 @@ public class ExpLogUtil {
         for (File file2 : files1) {
             BufferedReader bufferedRead = new BufferedReader(new FileReader(file2));
             String str = "";
+            int j =1;
 //readLine判断一行是以\r\n来判断的。如果最后一段字符没有\r\n，那么采用这种方式将无法读出剩下的字符串
             while ((str = bufferedRead.readLine()) != null) {
+                j++;
+                if (j>180132)
                 log.debug("list:{}", str);
                 list.add(str);
             }
 
         }
-        for (File expFile : files) {
-            System.out.println(expFile);
-            try {
-                BufferedReader bufferedRead = new BufferedReader(new FileReader(expFile));
-                while (bufferedRead.read() != -1) {
-                    String readLine = bufferedRead.readLine();
-                    for (int i = 0; i <list.size() ; i++) {
-//                        System.out.println(list.get(i));
-//                        System.out.println(readLine);
-                        if (readLine.contains(list.get(i))) {
-                            log.debug("readLine:{}",readLine);
-                        }else {
-
-                        }
-
-//                        String exp_error = readLine.substring(readLine.indexOf("TencentExp曝光的url值:/"));
-//                        String substring = exp_error.substring(exp_error.indexOf("bidid="));
-
-//                        HttpRequestUtil.sendGet("http://pixel.shuzijz.cn/tencentexp",substring);
-                    }
-                }
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
+//        for (File expFile : files) {
+//            System.out.println(expFile);
+//            try {
+//                BufferedReader bufferedRead = new BufferedReader(new FileReader(expFile));
+//                while (bufferedRead.read() != -1) {
+//                    String readLine = bufferedRead.readLine();
+//                    for (int i = 0; i <list.size() ; i++) {
+////                        System.out.println(list.get(i));
+////                        System.out.println(readLine);
+//                        if (readLine.contains(list.get(i))) {
+//                            log.debug("readLine:{}",readLine);
+//                        }else {
+//
+//                        }
+//
+////                        String exp_error = readLine.substring(readLine.indexOf("TencentExp曝光的url值:/"));
+////                        String substring = exp_error.substring(exp_error.indexOf("bidid="));
+//
+////                        HttpRequestUtil.sendGet("http://pixel.shuzijz.cn/tencentexp",substring);
+//                    }
+//                }
+//            } catch (FileNotFoundException e) {
+//                e.printStackTrace();
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        }
     }
 }
