aa4cd4efebb2f54ff79e174bf310b1a81d5ca475
dengjian <dj.deng@qq.com>
2019-03-27 10:58:11 +0800

    增加媒体，设备，广告位黑名单开关

--- src/main/java/cn/shuzilm/backend/timing/rtb/BlackListTask.java
+++ src/main/java/cn/shuzilm/backend/timing/rtb/BlackListTask.java
@@ -1,6 +1,7 @@
 package cn.shuzilm.backend.timing.rtb;
 
 import cn.shuzilm.backend.rtb.RtbFlowControl;
+import cn.shuzilm.common.AppConfigs;
 import cn.shuzilm.util.AdTagBlackListUtil;
 import cn.shuzilm.util.AppBlackListUtil;
 import cn.shuzilm.util.DeviceBlackListUtil;
@@ -14,11 +15,30 @@ import org.quartz.JobExecutionException;
  */
 @DisallowConcurrentExecution
 public  class BlackListTask implements Job {
+
+    private static final String FILTER_CONFIG = "filter.properties";
+    private static AppConfigs configs = AppConfigs.getInstance(FILTER_CONFIG);
+
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
-        AppBlackListUtil.updateAppBlackList();
-        DeviceBlackListUtil.updateDeviceBlackList();
-        AdTagBlackListUtil.updateAdTagBlackList();
+        // 开启媒体黑名单
+        if (Boolean.valueOf(configs.getString("BUNDLE_BLACK_LIST"))) {
+            AppBlackListUtil.updateAppBlackList();
+        }else {
+            AppBlackListUtil.stopTask();
+        }
+        // 开启设备黑名单
+        if (Boolean.valueOf(configs.getString("DEVICE_ID_BLACK_LIST"))) {
+            DeviceBlackListUtil.updateDeviceBlackList();
+        }else {
+            DeviceBlackListUtil.stopTask();
+        }
+        // 开启广告位黑名单
+        if (Boolean.valueOf(configs.getString("AD_TAG_BLACK_LIST"))) {
+            AdTagBlackListUtil.updateAdTagBlackList();
+        }else {
+            AdTagBlackListUtil.stopTask();
+        }
     }
 
     /*public static void main(String[] args) {
